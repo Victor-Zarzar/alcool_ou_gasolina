c9cccf095a3daf4a65991575aeffcc5618d357c0
victorzarzar <victorzarzar58@gmail.com>
2024-05-26 18:32:08 -0300

    Feat: add uiProvider dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,38 @@
+import 'package:alcool_ou_gasolina/components/DarkTheme/darktheme_provider_app.dart';
 import 'package:alcool_ou_gasolina/pages/IntroPage/intro_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: false,
+    return ChangeNotifierProvider(
+      create: (BuildContext context) => UiProvider()..init(),
+      child: Consumer<UiProvider>(
+        builder: (context, UiProvider notifier, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              useMaterial3: true,
+            ),
+            home: const IntroPage(),
+            themeMode: notifier.isDark ? ThemeMode.dark : ThemeMode.light,
+            darkTheme:
+                notifier.isDark ? notifier.darkTheme : notifier.lightTheme,
+          );
+        },
       ),
-      home: const IntroPage(),
     );
   }
 }
-

560fa3be3a27d8e6470215e91a6c60c5f698e8f9
victorzarzar <victorzarzar58@gmail.com>
2024-05-27 08:58:20 -0300

    Feat: create settings page and aboutpage

--- lib/pages/SettingsPage/settings_app.dart
+++ lib/pages/SettingsPage/settings_app.dart
@@ -1,4 +1,10 @@
+import 'package:alcool_ou_gasolina/components/AppTheme/app_theme.dart';
+import 'package:alcool_ou_gasolina/components/DarkTheme/darktheme_provider_app.dart';
+import 'package:alcool_ou_gasolina/pages/AboutPage/about_page.dart';
 import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -10,6 +16,79 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Consumer<UiProvider>(
+      builder: (context, notifier, child) {
+        return SizedBox(
+          child: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    GFAvatar(
+                      backgroundColor: notifier.isDark
+                          ? AppTheme.secondaryColor
+                          : AppTheme.primaryColor,
+                      radius: 80.0,
+                      backgroundImage: const AssetImage(
+                        "assets/gasoline.png",
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 40),
+                  child: ListTile(
+                    trailing: Switch(
+                      activeColor: Colors.black,
+                      inactiveTrackColor: Colors.white,
+                      value: notifier.isDark,
+                      onChanged: (value) => notifier.changeTheme(),
+                    ),
+                    leading: const Icon(Icons.dark_mode),
+                    title: Text(
+                      'darkmode',
+                      style: GoogleFonts.jetBrainsMono(
+                        textStyle: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: notifier.isDark
+                              ? TextColor.secondaryColor
+                              : TextColor.primaryColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                ListTile(
+                  leading: const Icon(Icons.info),
+                  title: Text(
+                    'about',
+                    style: GoogleFonts.jetBrainsMono(
+                      textStyle: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: notifier.isDark
+                            ? TextColor.secondaryColor
+                            : TextColor.primaryColor,
+                      ),
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const AboutPage(),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 }
